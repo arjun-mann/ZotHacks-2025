fd62ecd97d751085f8c7b66cc264a18389e68629
A2xaurt <A2xaurt@gmail.com>
2025-11-08 18:08:01 -0800

    Added WorkoutDisplay

--- quick-react/src/routes.jsx
+++ quick-react/src/routes.jsx
@@ -1,12 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './Home.jsx';
 import Gym from './Gym.jsx';
+import WorkoutDisplay from "./WorkoutDisplay.jsx";
+
 
 function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/gym-choice" element={<Gym />} />
+      <Route path="/workout" element={<WorkoutDisplay />} />
     </Routes>
   );
 }
