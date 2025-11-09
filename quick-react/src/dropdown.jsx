690aa2cfe2122f67b8fd860636adfd414db660db
Ayaan Mupparthi <120342231+AyaanMupparthi@users.noreply.github.com>
2025-11-08 17:14:22 -0800

    Update dropdown.jsx
    The dropdown component now takes a javascript array of strings

--- quick-react/src/dropdown.jsx
+++ quick-react/src/dropdown.jsx
@@ -3,21 +3,13 @@ import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 
 
-export default function Choice(options){
+export default function Choice({options}){
     return(
 
         <select name="selectedGoal">
-            {/* {options.map((item, index) => (
-                <option value="action-1">{item}</option>
-            ))} */}
-
-            <option value="action-1">{options[0]}</option>
-            <option value ="action-2">{options[1]}</option>
-            <option value = "action-3">{options[2]}</option>
-
-            <option value = "action-1">opt a</option>
-            <option value = "action-2">opt b</option>
-            <option value = "action-3">opt c</option>
+            {options.map((item, index) => (
+                <option value={item}>{item}</option>
+            ))}
         </select>
     );
 }
